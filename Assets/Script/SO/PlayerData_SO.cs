e368fcc32428d4e746972c35510a2e7864a1c7a2
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-03-18 14:08:38 +0900

    //Fix - Data SO Type

--- Assets/Script/SO/PlayerData_SO.cs
+++ Assets/Script/SO/PlayerData_SO.cs
@@ -12,15 +12,15 @@ public class PlayerData_SO : ScriptableObject
     [SerializeField]
     private int MaxArtifactCarry = 0;
     [SerializeField]
-    private float atk = 0f;
+    private float hp = 0;
     [SerializeField]
-    private float def = 0f;
+    private float atk = 0f;
     [SerializeField]
     private float speed = 0f;
 
     public int MaxLevel => maxLevel;
     public IReadOnlyList<int> RequireEXP => requireEXP;
+    public float HP => hp;
     public float ATK => atk;
-    public float DEF => def;
     public float Speed => speed;
 }
