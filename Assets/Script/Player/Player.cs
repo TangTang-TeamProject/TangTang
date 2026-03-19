1373d7eceaf02d583b7136f4fb335e5aadef81c3
HwangInsu99 <158982721+HwangInsu99@users.noreply.github.com>
2026-03-19 16:51:46 +0900

    Feat : Player
    테스트용도

---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IDamagables
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Hit(float damage)
+    {
+        Debug.Log("플레이어 맞았음");
+    }
+
+    public void Die()
+    {
+
+    }
+}
