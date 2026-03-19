2aa2d4b511e8e35bd7da4e3976e27cebb7c87a54
Iaminjae <wjsalswp303@gmail.com>
2026-03-19 13:46:45 +0900

    FEAT : 적 스포너

--- Assets/Script/Enemy/Spawn/BaseEnemyFactory.cs
+++ Assets/Script/Enemy/Spawn/BaseEnemyFactory.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public abstract class BaseEnemyFactory : MonoBehaviour
 {
-
-    public abstract BaseEnemy CreateEnemy();
+    public abstract BaseEnemy CreateEnemy(Vector2 position);
     
 }
