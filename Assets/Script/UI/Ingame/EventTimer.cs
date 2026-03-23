b9fd9daa883c90f2682514a720356c6b2e50aee1
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-03-23 18:10:54 +0900

    //FEAT - Ingame UI

---
+++ Assets/Script/UI/Ingame/EventTimer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTimer : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
