2a55def35bb4ebad8e75205644dcc5223d4a5577
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-03-23 18:38:26 +0900

    //FEAT - ingame Timer

--- Assets/Script/UI/Ingame/EventTimer.cs
+++ Assets/Script/UI/Ingame/EventTimer.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EventTimer : MonoBehaviour
 {
-    void Start()
+    [SerializeField]
+    private TextMeshProUGUI timeText;
+    [SerializeField]
+    private float eventCycle;
+
+
+    private void Update()
     {
-        
+        timeText.text = ((int)Timer.Instance.GameTime).ToString();
     }
 
-    void Update()
-    {
-        
-    }
 }
