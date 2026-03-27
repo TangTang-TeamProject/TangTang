ef0deb229c84b8a23cd8f376b87e76a36178e465
HwangInsu99 <158982721+HwangInsu99@users.noreply.github.com>
2026-03-27 14:18:18 +0900

    Refactor : Timer
    보스 출현시간 이벤트

--- Assets/Script/Shared/Timer.cs
+++ Assets/Script/Shared/Timer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,17 @@ public class Timer : MonoBehaviour
     // 시간관리 매니저 -> 보스 소환 이벤트줘야함 SO에서 이벤트 타임 받아야함
     public static Timer Instance {  get; private set; }
     private bool _isTimerRun = true;
-    private bool _isFirst = true;
     private float _timer;
     private float _realTime;
     private float _keepTime;
     private float _tickTime;
+    private int[] _bossTime = {300, 600 ,900};
+    private int _bossCount = 0;
 
     public float GameTime => _timer;
     public float RealTime => _realTime;
     public float TickTime => _tickTime;
+    public event Action BossSpawn;
 
     private void Awake()
     {
@@ -46,16 +49,19 @@ public class Timer : MonoBehaviour
         // 플레이 타이머 작동 여부
         if (!_isTimerRun)
         {
-            // 보스 출현시 시간 고정
-            if (_isFirst)
-            {
-                _keepTime = _timer / 60;
-                _timer = _keepTime;
-                _isFirst = false;
-            }
             return;
         }
         _timer += Time.deltaTime;
+
+        if (_timer >= _bossTime[_bossCount] && _bossCount < _bossTime.Length)
+        {
+            _isTimerRun = false;
+            _bossCount++;
+            BossSpawn?.Invoke();
+            // 보스 출현시 시간 고정
+            _keepTime = _timer / 60;
+            _timer = _keepTime;
+        }
     }
 
     public void IsTimeStop(bool stop)
@@ -63,15 +69,8 @@ public class Timer : MonoBehaviour
         Time.timeScale = stop ? 0.0f : 1.0f;
     }
 
-    public void IsBossRound(bool alive)
+    public void IsBossDie()
     {
-        _isTimerRun = !alive;
-        /* 어차피 보스가 죽던 출현하던 _isFirst는 true로 초기화를 해야하지만 예외처리를 하고 싶을 경우 주석해제
-        if (_isTimerRun)
-        {
-            _isFirst = true;
-        }
-        */
-        _isFirst = true;
+        _isTimerRun = true;
     }
 }
