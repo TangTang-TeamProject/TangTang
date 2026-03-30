e1a1923b8e919a96e8cc062a23d2585ed7a799c9
HwangInsu99 <158982721+HwangInsu99@users.noreply.github.com>
2026-03-30 18:33:04 +0900

    Feat : Axe
    회전도끼
    코루틴 분할

--- Assets/Script/Player/Skill/Axe.cs
+++ Assets/Script/Player/Skill/Axe.cs
@@ -1,11 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Axe : SkillAttack
 {
+    [SerializeField] private Transform _spawner;
+    [SerializeField] private float _radius = 2.0f;
+    private float _timer;
+    private float _offset;
+    private void OnEnable()
+    {
+        // 임시 실험용 나중에 구조 고치면서 스포너에서 넘겨줄거임
+        GameObject spawner = GameObject.Find("SkillSpawner");
+        _spawner = spawner.transform;
+        //
+        _isSpin = true;
+        _spinZ = 360f;
+        _timer = 0;
+    }
+
+    public override void SetOrbit(float dist)
+    {
+        _offset = dist * Mathf.Deg2Rad;
+    }
+
     protected override void Move()
     {
+        _timer += Time.deltaTime * _speed;
 
+        float targetPos = _timer + _offset;
+
+        float x = Mathf.Cos(targetPos) * _radius;
+        float y = Mathf.Sin(targetPos) * _radius;
+
+        transform.position = _spawner.position + new Vector3(x, y, 0);
+    }
+
+    protected override void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 0, _spinZ) * Time.deltaTime, Space.World);
     }
 }
