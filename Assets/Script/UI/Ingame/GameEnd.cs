b0aecdeca78ca38dff5403b049e970aeb29ae2ae
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-03-31 14:58:53 +0900

    //FEAT - GameEndUI

---
+++ Assets/Script/UI/Ingame/GameEnd.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEnd : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI endStatsText;
+    [SerializeField]
+    private TextMeshProUGUI earnCoinText;
+    [SerializeField]
+    private Button reGameBTN;
+    [SerializeField]
+    private Button menuBTN;
+}
