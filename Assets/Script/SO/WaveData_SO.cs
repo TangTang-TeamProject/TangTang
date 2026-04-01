56f1cf5885d1853f3446fff936f7323e79edcaee
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-04-01 12:51:18 +0900

    //FEAT - wave_SO

--- Assets/Script/SO/WaveData_SO.cs
+++ Assets/Script/SO/WaveData_SO.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveData_SO : MonoBehaviour
+[CreateAssetMenu(menuName = "GameData / WaveData", fileName = "WaveDataSO")]
+public class WaveData_SO : ScriptableObject
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private string stageID = "";
+    [SerializeField]
+    private int startSec = 0;
+    [SerializeField]
+    private int endSec = 0;
+    [SerializeField]
+    private string spawnEnemy = "";
+    [SerializeField]
+    private int enemyCount = 0;
+    [SerializeField]
+    private int spawnIntervalSec = 0;
 
-    void Update()
-    {
-        
-    }
+    public string StageID => stageID;
+    public int StartSec => startSec;
+    public int EndSec => endSec;
+    public int EnemyCount => enemyCount;
+    public string SpawnEnemy => spawnEnemy;
+    public int SpawnIntervalSec => spawnIntervalSec;
 }
--- Assets/Script/SO/WaveRegistry.cs
+++ Assets/Script/SO/WaveRegistry.cs
@@ -2,15 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveRegistry : MonoBehaviour
+[CreateAssetMenu(menuName = "GameData / WaveRegistry", fileName = "WaveRegistrySO")]
+public class WaveRegistry : ScriptableObject
 {
-    void Start()
+    [SerializeField]
+    private List<WaveData_SO> waves = new List<WaveData_SO>();
+
+    public IReadOnlyList<WaveData_SO> Waves => waves;
+
+    private Dictionary<string, List<WaveData_SO>> dataDic = new Dictionary<string, List<WaveData_SO>>();
+
+    void NullCheck()
     {
-        
+        if (dataDic != null && dataDic.Count != 0)
+        {
+            return;
+        }
+
+        MakeDic();
+    }
+    public void MakeDic()
+    {
+        dataDic.Clear();
+
+        for (int i = 0; i < waves.Count; i++)
+        {
+            if (!dataDic.ContainsKey(waves[i].StageID))
+            {
+                dataDic.Add(waves[i].StageID, new List<WaveData_SO>());
+            }
+
+            dataDic[waves[i].StageID].Add(waves[i]);
+        }
     }
 
-    void Update()
+    public List<WaveData_SO> GetEnemyByID(string _ID)
     {
-        
+        NullCheck();
+
+        if (dataDic.TryGetValue(_ID, out List<WaveData_SO> data))
+        {
+            return data;
+        }
+
+        CPrint.Error("WaveRegistry - Cant Find");
+        return null;
     }
 }
