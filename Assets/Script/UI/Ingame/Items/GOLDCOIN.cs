a0c23a8fff24df8470a382339b4dfa49e1f20b85
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-04-07 18:05:33 +0900

    //Feat - Item Use

--- Assets/Script/UI/Ingame/Items/GOLDCOIN.cs
+++ Assets/Script/UI/Ingame/Items/GOLDCOIN.cs
@@ -1,8 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GOLDCOIN : Items
 {
+    public Action GetEaten;
 
+    private void Awake()
+    {
+        GetEaten += ItemManager.instance.ShowMeTheMoney;
+    }
+
+
+    public override void SetActiveFalse()
+    {
+        GetEaten?.Invoke();
+        GetEaten = null;
+        Destroy(this.gameObject);
+    }
 }
--- Assets/Script/UI/Ingame/Items/MAGNATIC.cs
+++ Assets/Script/UI/Ingame/Items/MAGNATIC.cs
@@ -1,8 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MAGNATIC : Items
 {
-    
+    public Action<GameObject> GetEaten;
+
+    private void Awake()
+    {
+        GetEaten += ItemManager.instance.LikeItsMagnetic;
+    }
+
+    public override void SetActiveFalse()
+    {
+        GetEaten?.Invoke(_target);
+        GetEaten = null;
+        Destroy(this.gameObject);
+    }
 }
