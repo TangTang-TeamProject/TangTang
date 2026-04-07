59343e449c7970f3d354f5156d626597991b0215
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-04-07 14:17:58 +0900

    //Feat - Skill Pick Event

--- Assets/Script/UI/Ingame/SkillPick.cs
+++ Assets/Script/UI/Ingame/SkillPick.cs
@@ -1,20 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillPick : MonoBehaviour
 {
     private Action callback;
 
-    [SerializeField]
-    private Transform pos_1;
-    [SerializeField]
-    private Transform pos_2;
-    [SerializeField]
-    private Transform pos_3;
+    [System.Serializable]
+    private class Choice
+    {
+        public Button choice;
+        public Image choice_back;
+        public Image choice_icon;
+        public TextMeshProUGUI choice_desc;
+        public string pickedID;
+    }
 
-    private string pickedID;
+
+    [SerializeField]
+    private List<Choice> choices;
+    [SerializeField]
+    private Sprite evoIMG;
+    [SerializeField]
+    private Sprite skillIMG;
+    [SerializeField]
+    private Sprite artiIMG;
+
+
+    private void Awake()
+    {
+        for (int i = 0; i < choices.Count; i++)
+        {
+            if (choices[i] == null)
+            {
+                CPrint.Error("인스펙터 참조 오류 - choices");
+            }
+
+            choices[i].choice.onClick.AddListener(EndPick);
+        }
+    }
 
     public void StartPick(Action _callback)
     {
