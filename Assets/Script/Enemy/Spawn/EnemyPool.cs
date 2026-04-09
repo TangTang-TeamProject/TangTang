f658d7ef2108386ecfd94d7a9ac83296f646ea7f
Iaminjae <wjsalswp303@gmail.com>
2026-04-09 16:41:00 +0900

    Fix : 젬 수정, 보스 체력바 수정,  STG_2 몬스터 제작

--- Assets/Script/Enemy/Spawn/EnemyPool.cs
+++ Assets/Script/Enemy/Spawn/EnemyPool.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPool : MonoBehaviour
-{
-
-    [SerializeField] private GameObject _enemyPrefab;
-
+{    
     private Queue<BaseEnemy> _enemyPool = new Queue<BaseEnemy>();
 
     public Action<BaseEnemy> OnEnemyDead;
