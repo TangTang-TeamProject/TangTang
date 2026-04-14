f5b73f0fbddc811fd744b1ff6c554088615d8449
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-04-14 14:02:05 +0900

    //Feat - stageData SO

---
+++ Assets/Script/SO/StageData_SO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameData / StageData", fileName = "StageDataSO")]
+public class StageData_SO : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
