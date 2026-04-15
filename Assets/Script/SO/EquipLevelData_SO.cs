5daaed3cb294978503684cb5d8d279385f1536b1
AngryNeeson-52 <102715399+AngryNeeson-52@users.noreply.github.com>
2026-04-15 13:51:56 +0900

    //Feat - Equip Grade

---
+++ Assets/Script/SO/EquipLevelData_SO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipLevelData_SO : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
